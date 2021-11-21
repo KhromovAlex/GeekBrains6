1710f5c44cbe8a35fc2b2ea96a809114654a0d83
Alexandr Khromov <4-09@bk.ru>
2021-11-21 23:04:56 +0700

    fix

--- app/src/main/java/com/example/gbapp6/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gbapp6/presentation/MainActivity.kt
@@ -1,30 +1,6 @@
 package com.example.gbapp6.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.example.gbapp6.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private var navController: NavController? = null
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//
-//        navController = findNavController(this, R.id.main_nav)
-//
-//        setupActionBarWithNavController(this, navController!!)
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        return navController?.navigateUp() == true || super.onSupportNavigateUp()
-//    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
